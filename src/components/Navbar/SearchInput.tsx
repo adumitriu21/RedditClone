bed9737ae471884e0a9815a0c156c69baf76167b
barnau21 <adumitriu21@gmail.com>
2023-05-30 22:08:30 +0300

    added a second drop down on the navbar, only visible when a user is logged in

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -7,15 +7,16 @@ import {
   InputRightElement,
   Stack,
 } from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
 
 type SearchInputProps = {
-  //user:
+  user?: User | null
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} align="center">
+    <Flex flexGrow={1} mr={2} align="center" maxWidth={ user ? 'auto' : '600px'}>
       
         <InputGroup>
           <InputLeftElement
