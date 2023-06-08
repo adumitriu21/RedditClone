56b787111df874762f951b880f03ac24686123ef
barnau21 <adumitriu21@gmail.com>
2023-06-08 10:47:02 +0300

    began implementing post tab, letting user decide what they want to post (with the use of tabs)

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import { Flex, Icon } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { IoDocumentText, IoImageOutline} from 'react-icons/io5'
+import { BsLink45Deg, BsMic} from 'react-icons/bs'
+import { BiPoll } from 'react-icons/bi'
+import TabItem from './TabItem';
 
 type NewPostFormProps = {
     
 };
 
+const formTabs = [
+    {
+        title: 'Post',
+        icon: IoDocumentText
+    },
+    {
+        title: 'Images & Video',
+        icon: IoImageOutline
+    },
+    {
+        title: 'Link',
+        icon: BsLink45Deg
+    },
+    {
+        title: 'Poll',
+        icon: BiPoll
+    },
+    {
+        title: 'Talk',
+        icon: BsMic
+    }
+];
+
+export type TabItem = {
+    title: string;
+    icon: typeof Icon.arguments;
+}
+
 const NewPostForm:React.FC<NewPostFormProps> = () => {
-    
-    return <div>Have a good coding</div>
+    const [selectedTab, setSelectedTab] = useState(formTabs[0].title)
+    return (
+        <Flex
+            direction='column'
+            bg='white'
+            borderRadius={4}
+            mt={2}
+        >
+            <Flex width='100%'>
+                {formTabs.map((item) => (
+                   // eslint-disable-next-line react/jsx-key
+                   <TabItem item={item} selected={item.title === selectedTab} setSelectedTab={setSelectedTab}/>
+                ))}
+            </Flex>
+        </Flex>
+        )
 }
 export default NewPostForm;
