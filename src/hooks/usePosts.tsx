e459eebd41faf4188a90293c10d44a9ee252412c
barnau21 <adumitriu21@gmail.com>
2023-06-14 23:11:05 +0300

    started working on the voting system logic

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,15 +1,61 @@
-import { deleteDoc, doc } from 'firebase/firestore';
+import { collection, deleteDoc, doc, writeBatch } from 'firebase/firestore';
 import { deleteObject, ref } from 'firebase/storage';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
-import { Post, postState } from '../atoms/postsAtom';
-import { firestore, storage } from '../firebase/clientApp';
+import { Post, postState, PostVote } from '../atoms/postsAtom';
+import { auth, firestore, storage } from '../firebase/clientApp';
 
 
 const usePosts = () => {
     const [postStateValue, setPostStateValue] = useRecoilState(postState)
-    
-    const onVote = async() => {};
+    const [user] = useAuthState(auth);
+    const onVote = async(post:Post, vote: number, communityId: string) => {
+        //check if user is logged in, if not open the auth modal
+        try {
+            const {voteStatus} = post;
+            const existingVote = postStateValue.postVotes.find(
+                (vote) => vote.postId === post.id
+            )
+            const batch = writeBatch(firestore)
+            const updatedPost = {...post}
+            const updatedPosts = [...postStateValue.posts]
+            const updatedPostVotes = [...postStateValue.posts]
+            let voteChange = vote;
+
+            //New vote
+            if(!existingVote){
+
+                const postVoteRef = doc(collection(firestore, 'users', `${user?.uid}/postVotes`))
+
+                const newVote: PostVote = {
+                    id: postVoteRef.id,
+                    postId: post.id!,
+                    communityId,
+                    voteValue: vote //1 or -1
+                }
+
+                batch.set(postVoteRef, newVote)
+                //add or subtract 1 from post.voteStatus
+                updatedPost.voteStatus = voteStatus + vote
+
+                updatedPostVotes = [...updatedPostVotes, newVote]
+                //create new postVote document
+            } else {
+                //the have voted on the document before
+                if(removingVote){
+                    // add or subtract 1 on the post.voteStatus
+                    // remove postVote document
+                } else {
+                    // add or subtract 2 on the post.voteStatus
+                    // updating the existing postVote document 
+                }
+            }
+        } catch (error) {
+            console.log('onVote error', error)
+        }
+        
+    };
     
     const onSelectPost = () => {};
     
