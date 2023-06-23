f14358524c77929ac3356d03fb2988c9be2092a3
barnau21 <adumitriu21@gmail.com>
2023-06-23 09:27:30 +0300

    implented fetching comments from DB on post

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,13 +1,25 @@
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  SkeletonCircle,
+  SkeletonText,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { User } from "firebase/auth";
 import {
   collection,
   doc,
+  getDocs,
   increment,
+  orderBy,
+  query,
   serverTimestamp,
   Timestamp,
+  where,
   writeBatch,
 } from "firebase/firestore";
+import { copyFile } from "fs";
 import React, { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { Post, postState } from "../../../atoms/postsAtom";
@@ -28,7 +40,7 @@ const Comments: React.FC<CommentsProps> = ({
 }) => {
   const [commentText, setCommentText] = useState("");
   const [comments, setComments] = useState<Comment[]>([]);
-  const [fetchLoading, setFetchLoading] = useState(false);
+  const [fetchLoading, setFetchLoading] = useState(true);
   const [createLoading, setCreateLoading] = useState(false);
   const setPostState = useSetRecoilState(postState);
 
@@ -54,7 +66,7 @@ const Comments: React.FC<CommentsProps> = ({
 
       batch.set(commentDocRef, newComment);
 
-      newComment.createdAt= { seconds: Date.now() / 1000} as Timestamp
+      newComment.createdAt = { seconds: Date.now() / 1000 } as Timestamp;
 
       //update the post's number of comments
       const postDocRef = doc(firestore, "posts", selectedPost?.id!);
@@ -86,11 +98,34 @@ const Comments: React.FC<CommentsProps> = ({
     //update client recoil state
   };
 
-  const getPostComments = async () => {};
+  const getPostComments = async () => {
+    try {
+      const commentsQuery = query(
+        collection(firestore, "comments"),
+        where("postId", "==", selectedPost?.id),
+        orderBy('createdAt', 'desc')
+      );
+
+      const commentDocs = getDocs(commentsQuery);
+
+      const comments = (await commentDocs).docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setComments(comments as Comment[]);
+    } catch (error) {
+      console.log("get post comments error: ", error);
+    }
+
+    setFetchLoading(false);
+  };
 
   useEffect(() => {
+    if(!selectedPost) return;
     getPostComments();
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedPost]);
   return (
     <Box bg="white" borderRadius="0px 0px 4px 4px" p={2}>
       <Flex
@@ -112,15 +147,45 @@ const Comments: React.FC<CommentsProps> = ({
         }
       </Flex>
       <Stack spacing={6} p={2}>
-        {comments.map((comment) => (
-          <CommentItem
-            key=''
-            comment={comment}
-            onDeleteComment={onDeleteComment}
-            loadingDelete={false}
-            userId={user.uid}
-          />
-        ))}
+        {fetchLoading ? (
+          <>
+            {[0, 1, 2].map((item) => (
+              <Box key={item} padding={6} bg="white">
+                <SkeletonCircle size="10" />
+                <SkeletonText mt={4} noOfLines={2} spacing={4} />
+              </Box>
+            ))}
+          </>
+        ) : (
+          <>
+            {comments.length === 0 ? (
+              <Flex
+                direction="column"
+                justify="center"
+                align="center"
+                borderTop="1px solid"
+                borderColor="gray.100"
+                p={20}
+              >
+                <Text fontWeight={700} opacity={0.3}>
+                  No comments yet.
+                </Text>
+              </Flex>
+            ) : (
+              <>
+                {comments.map((comment) => (
+                  <CommentItem
+                    key=""
+                    comment={comment}
+                    onDeleteComment={onDeleteComment}
+                    loadingDelete={false}
+                    userId={user.uid}
+                  />
+                ))}
+              </>
+            )}
+          </>
+        )}
       </Stack>
     </Box>
   );
